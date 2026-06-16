29e8e0144f8eaebbd565cf7c6219439cd84d667f
ericmeekins <xiau0981@gmail.com>
2026-06-16 10:17:58 +0700

    update

--- wp-content/languages/loco/themes/outdoor-camping-zh_CN.l10n.php
+++
@@ -1,3 +0,0 @@
-<?php
-return ['project-id-version'=>'Outdoor Camping 1.0.0','report-msgid-bugs-to'=>'https://wordpress.org/support/theme/outdoor-camping','last-translator'=>'camtravel damin','language-team'=>'简体中文','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2025-09-10T15:35:56+05:30','po-revision-date'=>'2025-09-30 04:26+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'outdoor-camping
-','language'=>'zh_CN','plural-forms'=>'nplurals=1; plural=0;','x-loco-version'=>'2.8.0; wp-6.8.2; php-8.4.10','messages'=>[]];
